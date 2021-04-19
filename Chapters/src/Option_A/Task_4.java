1a99151a5dc753a9b3a47c39282d9b87458b7fe9
Mdzhamalov <M_Djamalov@mail.ru>
2021-04-19 19:04:00 +0300

    fix task 4

--- Chapters/src/Option_A/Task_4.java
+++ Chapters/src/Option_A/Task_4.java
@@ -2,25 +2,32 @@ package Option_A;
 
 import java.util.Scanner;
 
-// Ввести пароль в командную строку.
-// Сравнить его со строкой-образцом.
+// Input password in command line
+// Compare password with inputed value
+
 public class Task_4 {
 
     public static void main(String[] args) {
-        Scanner password = new Scanner(System.in);
-        System.out.print("Input your password: ");
 
-        String actual_password = password.next(); // read the value
-        String valid_password = "1111";
-
-        if (actual_password.equals(valid_password))
-            System.out.println("The password is correct");
-        else
-            System.out.println("Wrong password");
+        for (int i = 0; i < args.length; i++) {
+            String valid_password = "1111";
+            if (args[i].equals(valid_password))
+                System.out.println("You entered correct password");
+            else
+                System.out.println("Wrong password. Try again");
+        }
 
 
-
-
+//        Scanner password = new Scanner(System.in);
+//        System.out.print("Input your password: ");
+//
+//        String actual_password = password.next(); // read the value
+//        String valid_password = "1111";
+//
+//        if (actual_password.equals(valid_password))
+//            System.out.println("The password is correct");
+//        else
+//            System.out.println("Wrong password");
     }
 
 }
