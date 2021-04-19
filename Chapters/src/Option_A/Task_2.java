d97cbf9ef7fc7120093e2d88c3411a20c0524988
Mdzhamalov <M_Djamalov@mail.ru>
2021-04-19 18:37:39 +0300

    fix task 2

---
+++ Chapters/src/Option_A/Task_2.java
@@ -0,0 +1,26 @@
+package Option_A;
+import java.util.Scanner;
+
+// Display the arguments of command line in reverse order
+
+public class Task_2 {
+
+    public static void main(String[] args) {
+
+        for (int i = args.length - 1; i >= 0; i--) {
+            System.out.println("Argument = " + args[i]);
+        }
+
+
+
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Input the word: ");
+//
+//        for (int i = 0; true; i++) { // condition is always true
+//            StringBuffer console = new StringBuffer(input.nextLine());
+//            console.reverse();
+//            System.out.println("Reversed: " + console.toString());
+        }
+
+    }
