ddb6abb2f3b58e4ab7cc1606eab431d73191568a
Mdzhamalov <M_Djamalov@mail.ru>
2021-04-21 12:42:06 +0300

    fix task_5

--- Chapters/src/Option_A/Task_5.java
+++ Chapters/src/Option_A/Task_5.java
@@ -9,19 +9,25 @@ import java.util.Scanner;
 public class Task_5 {
 
     public static void main(String[] args) {
-
-        Scanner numbers = new Scanner(System.in);
-        System.out.println("Input numbers: ");
-
-        for (int i = 0; true; i++) {
-            int a = numbers.nextInt();
-            int b = numbers.nextInt();
-
-            System.out.println("Sum is: " + (a + b));
-            System.out.println("Multiplication is: " + (a * b));
+        int sum = 0;
+        int mult = 1;
+        for (int i = 0; i < args.length; i++) {
+            sum += Integer.valueOf(args[i]);
+            mult *= Integer.valueOf(args[i]);
         }
+        System.out.println("Sum is: " + sum);
+        System.out.println("Multiplication is: " + mult);
 
-
+//        Scanner numbers = new Scanner(System.in);
+//        System.out.println("Input numbers: ");
+//
+//        for (int i = 0; true; i++) {
+//            int a = numbers.nextInt();
+//            int b = numbers.nextInt();
+//
+//            System.out.println("Sum is: " + (a + b));
+//            System.out.println("Multiplication is: " + (a * b));
+//        }
 
     }
 }
