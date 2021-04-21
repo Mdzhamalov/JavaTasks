1b67470bdcd3d82df3aeb43da8e492af4d54e2fc
Mdzhamalov <M_Djamalov@mail.ru>
2021-04-21 18:32:14 +0300

    Update task_B_1

--- Chapters/src/Option_B/Task_B_1.java
+++ Chapters/src/Option_B/Task_B_1.java
@@ -8,20 +8,20 @@ public class Task_B_1 {
 
     public static void main(String[] args) {
 
-        // System.out.println("Введите желаемое количество целых чисел в массиве: ");
+
         Scanner input = new Scanner(System.in);
-        // int size = input.nextInt();
-        int size = 5;
-        System.out.println("Введите " + size + " целых чисел через пробел: ");
+        System.out.println("Enter array length: ");
+        int size = input.nextInt(); // read the array and write them to size
         int[] Numbers = new int[size];
+        System.out.println("Insert array elements: ");
 
 
         for (int i = 0; i < size; i++) {
             Numbers[i] = input.nextInt();
             if (Numbers[i] % 2 == 0)
-                System.out.println("Чётное число: " + Numbers[i]);
+                System.out.println("Even number: " + Numbers[i]);
             else if (Numbers[i] % 2 != 0)
-                System.out.println("Нечётное число: " + Numbers[i]);
+                System.out.println("Uneven number: " + Numbers[i]);
         }
 
     }
