efedc7d66d83ee1f6925649431b92b17ae527b86
Mdzhamalov <M_Djamalov@mail.ru>
2021-04-22 18:30:48 +0300

    update task_b_4

--- Chapters/src/Option_B/Task_B_4.java
+++ Chapters/src/Option_B/Task_B_4.java
@@ -9,15 +9,15 @@ public class Task_B_4 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int n = 6;
-        System.out.print("Введите " + n + " целых чисел через пробел: ");
-
+        System.out.println("Input array length: ");
+        int n = input.nextInt();
         int[] numbers = new int[n];
+        System.out.println("Insert array elements: ");
 
         for (int i = 0; i < n; i++) {
             numbers[i] = input.nextInt();
             if ((numbers[i] % 5 == 0) && (numbers[i] % 7 == 0))
-                System.out.println("делится на 5 и на 7:  " + numbers[i]);
+                System.out.println("Divided by 5 and 7:  " + numbers[i]);
         }
 
     }
